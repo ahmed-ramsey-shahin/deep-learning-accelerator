035d31207b455c3e1fba2e14dc317a8c3a29d9ae
Ziad Hamdy <139954179+ZiadHamdyMohammed@users.noreply.github.com>
2025-06-06 04:29:25 +0300

    Implemented ReLU activation function

--- RTL/Activations/ReLU/ReLU.v
+++ RTL/Activations/ReLU/ReLU.v
@@ -1,9 +1,13 @@
-module ReLU (in, out);
-    parameter WIDTH = 32;
-    
-    input [WIDTH-1:0] in;
-    output [WIDTH-1:0] out;
+module ReLU (in, out);    
+    input signed [23:0] in;
+    output reg signed [7:0] out;
 
-    assign out = (in[WIDTH-1] == 1'b1) ? {WIDTH{1'b0}} : in;
+    always @(*) begin
+        if (in[23] == 1'b1) begin
+            out = 0;
+        end else begin
+            out = (in >> 16);
+        end
+    end
 
 endmodule
