d123ee90f1e78a511462c962ec6d42f4cc66d65c
Ziad Hamdy <139954179+ZiadHamdyMohammed@users.noreply.github.com>
2025-08-22 00:57:09 +0300

    Testbench for ReLU

--- RTL/Activations/ReLU/ReLU_tb.v
+++ RTL/Activations/ReLU/ReLU_tb.v
@@ -1,32 +1,38 @@
-module ReLU_tb ();    
-    reg signed [23:0] in;
-    wire signed [7:0]out;
+module ReLU_tb ();
+    localparam IN_WIDTH = 8;
+    localparam OUT_WIDTH = 8;
+    reg signed [IN_WIDTH-1:0] in;
+    reg en;
+    wire signed [OUT_WIDTH-1:0]out;
 
-    ReLU dut(in, out);
+    ReLU #(.IN_WIDTH(IN_WIDTH), .OUT_WIDTH(OUT_WIDTH)) dut(.in(in), .en(en), .out(out));
 
     initial begin
+        en = 1;
         in = 5;
         #10;
         
-        in = 200;
+        in = 100;
         #10;
         
-        in = 65535;
-        #10;
-        
-        in = 65536;
-        #10;
-
-        in = 6553600;
-        #10;
-        
-        in = 8388607;
+        in = 127;
         #10;
         
         in = -5;
         #10;
+        
+        in = -100;
+        #10;
+        
+        in = -128;
+        #10;
 
-        in = -8388608;
+        en = 0;
+        in = 5;
+        #10;
+
+        en = 1;
+        in = 15;
         #10;
     end
 
