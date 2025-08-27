3e09c81f04638015eff1cf1f52d71725408f36a8
Ziad Hamdy <139954179+ZiadHamdyMohammed@users.noreply.github.com>
2025-08-27 20:21:13 +0300

    Testbench for Sigmoid

--- RTL/Activations/Sigmoid/Sigmoid_tb.sv
+++ RTL/Activations/Sigmoid/Sigmoid_tb.sv
@@ -1,62 +1,22 @@
 module Sigmoid_tb ();
-    parameter IN_WIDTH = 15;
-    parameter OUT_WIDTH = 11;
-    reg signed [IN_WIDTH-1:0] in;
+    parameter DATA_WIDTH = 12;
+    parameter SA_LENGTH = 7;
+    parameter S = 7;
+    reg signed [DATA_WIDTH-1:0] in [SA_LENGTH];
     reg en;
-    wire signed [OUT_WIDTH-1:0] out;
+    wire signed [DATA_WIDTH-1:0] out [SA_LENGTH];
 
-    reg signed [IN_WIDTH-1:0] test_inputs [0:10] = '{
-        0,
-        5,
-        300,
-        1600,
-        2433,
-        8000,
-        -5,
-        -300,
-        -1600,
-        -2433,
-        -8000
-    };
-    
-    reg [OUT_WIDTH-1:0] expected_outputs [0:10] = '{
-        511,        // 0
-        512,        // 5
-        586,        // 300
-        839,        // 1600
-        939,        // 2433
-        1023,       // 8000
-        511,        // -5
-        437,        // -300
-        184,        // -1600
-        84,         // -2433
-        0           // -8000
-    };
+    Sigmoid #(.DATA_WIDTH(DATA_WIDTH), .SA_LENGTH(SA_LENGTH), .S(S)) dut (.in(in), .en(en), .out(out));
 
-    Sigmoid #(.IN_WIDTH(IN_WIDTH), .OUT_WIDTH(OUT_WIDTH)) dut (.in(in), .en(en), .out(out));
-
-    integer i;
     initial begin
         en = 1;
-        for (i = 0; i < 11; i = i + 1) begin
-            in = test_inputs[i];
-            #10; // Wait for propagation
-            
-            // Check output
-            if (out !== expected_outputs[i]) begin
-                $display("Error [Test %0d]: Input = %0d, Expected = %0d, Got = %0d", i, in, expected_outputs[i], out);
-            end
-        end
-
-        en = 0;
-        in = 5;
+        in = {0, 400, 517, -512, -1, -2048, 2047};
         #10;
 
-        in = 15;
+        en = 0;
         #10;
 
         en = 1;
-        in = 0;
         #10;
     end
 endmodule
