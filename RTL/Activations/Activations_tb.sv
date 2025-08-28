f2f7cfaf0bfdf37483be1324f2f6c49e81cc17a4
Ziad Hamdy <139954179+ZiadHamdyMohammed@users.noreply.github.com>
2025-08-28 12:01:58 +0300

    Testbench for the top module of the activation functions

--- RTL/Activations/Activations_tb.sv
+++ RTL/Activations/Activations_tb.sv
@@ -3,11 +3,11 @@ module Activations_tb ();
     parameter SA_LENGTH = 8;
     parameter S = 7;
     reg signed [DATA_WIDTH-1:0] in [SA_LENGTH];
-    reg clk;
+    reg clk, en;
     reg [1:0] sel;
     wire signed [DATA_WIDTH-1:0] out [SA_LENGTH];
 
-    Activations #(.DATA_WIDTH(DATA_WIDTH), .SA_LENGTH(SA_LENGTH), .S(S)) dut (.in(in), .clk(clk), .sel(sel), .out(out));
+    Activations #(.DATA_WIDTH(DATA_WIDTH), .SA_LENGTH(SA_LENGTH), .S(S)) dut (.in(in), .clk(clk), .en(en), .sel(sel), .out(out));
 
     initial begin
         clk = 0;
@@ -19,6 +19,7 @@ module Activations_tb ();
 
     initial begin
         @(negedge clk);
+        en = 1;
         in = {0, 400, 517, -512, -1, -2048, 2047, 52};
         sel = 2'b00;
         @(negedge clk);
@@ -33,6 +34,10 @@ module Activations_tb ();
         @(negedge clk);
 
         sel = 2'b00;
+        @(negedge clk);
+
+        en = 0;
+        sel = 2'b01;
         @(negedge clk);
         $stop;
     end
