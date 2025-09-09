fd1e562f3262742b637e6e528d9ac8b3081415a9
ahmed-ramsey-shahin <ahmed.ramsey.shahin@yandex.com>
2025-09-09 22:49:58 +0300

    Fixing ToRight signal in the processing element
    after loading the SA with weights, the ToRight signal keeps the data
    registered which causes the SA to produce wrong results. The issue is
    now fixed by adding two  ToRight registers, one for loading and one for
    normal operation.

--- RTL/Processing_Element/Processing_Element.v
+++ RTL/Processing_Element/Processing_Element.v
@@ -12,27 +12,40 @@ module Processing_Element #(
     output reg  signed [DATA_WIDTH-1:0]             ToRight,
     output reg  signed [ACCUMULATOR_DATA_WIDTH-1:0] PsumOut
 );
-    reg  signed [DATA_WIDTH-1:0]   registered_weight;
+    reg signed [DATA_WIDTH-1:0] registered_weight;
+    reg signed [DATA_WIDTH-1:0] WeightToRight;
+    reg signed [DATA_WIDTH-1:0] InputToRight;
+
+    always @(*) begin
+        if (LOAD) begin
+            ToRight = WeightToRight;
+        end
+        else begin
+            ToRight = InputToRight;
+        end
+    end
 
     always @(posedge CLK or negedge ASYNC_RST) begin
         if (~ASYNC_RST) begin
             registered_weight <= 'd0;
-            ToRight           <= 'd0;
+            WeightToRight     <= 'd0;
+            InputToRight      <= 'd0;
             PsumOut           <= 'd0;
         end
         else if (SYNC_RST) begin
             registered_weight <= 'd0;
-            ToRight           <= 'd0;
+            WeightToRight     <= 'd0;
+            InputToRight      <= 'd0;
             PsumOut           <= 'd0;
         end
         else if (EN) begin
             if (LOAD) begin
                 registered_weight <= Input;
-                ToRight           <= Input;
+                WeightToRight     <= Input;
             end
             else begin
-                PsumOut <= (Input * registered_weight) + PsumIn;
-                ToRight <= Input;
+                PsumOut      <= (Input * registered_weight) + PsumIn;
+                InputToRight <= Input;
             end
         end
     end
